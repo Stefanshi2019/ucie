595213b7872a39a8d13c43f7f58b1cd2c37d4bb6
Stefanshi2019 <stefan_shi@berkeley.edu>
2024-02-10 21:14:35 -0800

    syntax-clean, completed sideband sender module, completed sender testbench, Untested, please do

--- src/main/scala/sideband/PacketType.scala
+++ src/main/scala/sideband/PacketType.scala
@@ -45,3 +45,87 @@ object PacketType extends ChiselEnum {
   /** Message with 64b Data */
   val MessageWith64bData = Value("b11011".U(5.W))
 }
+
+
+
+  // Link training message too much, will do later, p148
+
+  // Side band message definitions, refer to p146
+object MsgCode extends ChiselEnum {
+  val Nop                   = Value(0x00.U(8.W))
+  val LinkMgmt_RDI_Req      = Value(0x01.U(8.W))
+  val LinkMgmt_RDI_Rsp      = Value(0x02.U(8.W))
+  val LinkMgmt_Adaptor0_Req = Value(0x03.U(8.W))
+  val LinkMgmt_Adaptor0_Rsp = Value(0x04.U(8.W))
+  val LinkMgmt_Adaptor1_Req = Value(0x05.U(8.W))
+  val LinkMgmt_Adaptor1_Rsp = Value(0x06.U(8.W))
+  val ParityFeature_Req     = Value(0x07.U(8.W))
+  val ParityFeature_Ack     = Value(0x08.U(8.W))
+  val ErrorMsg              = Value(0x09.U(8.W))
+}
+
+
+// // To Do, Chisel doesn't allow two mappings to the same value, need to change to this
+// object MyEnum extends ChiselEnum {
+//   val Req, Ack, Correctable, Crd, Nak, NonFatal, Active, Fatal, PMNAK, L1, L2, LinkReset, LinkError, Retrain, Disable = Value
+// }
+
+// val logicalValueMap: Map[MyEnum.Type, UInt] = Map(
+//   MyEnum.Req -> 0.U,
+//   MyEnum.Ack -> 0.U,
+//   MyEnum.Correctable -> 0.U,
+//   MyEnum.Crd -> 0.U,
+//   // Add other mappings
+//   MyEnum.Nak -> 1.U,
+//   MyEnum.NonFatal -> 1.U,
+//   MyEnum.Active -> 1.U,
+//   // Continue for other values
+// )
+
+
+  // Side band message definitions, refer to p146
+object MsgSubCode extends ChiselEnum {
+
+
+  val Crd       = Value(0x00.U(8.W))  // Nop 0x00
+  val Active    = Value(0x01.U(8.W))  // LinkMgmt 0x01 - 0x06
+
+  val PMNAK     = Value(0x02.U(8.W))  // LinkMgmt 0x01 - 0x06
+  val L1        = Value(0x04.U(8.W))  // LinkMgmt 0x01 - 0x06
+  val L2        = Value(0x08.U(8.W))  // LinkMgmt 0x01 - 0x06
+  val LinkReset = Value(0x09.U(8.W))  // LinkMgmt 0x01 - 0x06
+  val LinkError = Value(0x0a.U(8.W))  // LinkMgmt 0x01 - 0x06
+  val Retrain   = Value(0x0b.U(8.W))  // LinkMgmt 0x01 - 0x06
+  val Disable   = Value(0x0c.U(8.W))  // LinkMgmt 0x01 - 0x06
+
+
+  val Req = Value(0x10.U(8.W))  // ParityFeature 0x07
+  val Nak = Value(0x11.U(8.W))  // ParityFeature 0x08 
+  val Ack = Value(0x12.U(8.W))  // ParityFeature 0x08
+
+  // // Chisel enum doesn't allow mapping to same value
+  // // Change to different value for now, will fix later
+  // // Original, commented for now
+  // val Req = Value(0x00.U(8.W))  // ParityFeature 0x07
+
+  // val Ack = Value(0x00.U(8.W))  // ParityFeature 0x08
+  // val Nak = Value(0x01.U(8.W))  // ParityFeature 0x08
+
+  // val Correctable = Value(0x00.U(8.W))  //ErrorMsg 0x09
+  // val NonFatal    = Value(0x01.U(8.W))  //ErrorMsg 0x09
+  // val Fatal       = Value(0x02.U(8.W))  //ErrorMsg 0x09
+}
+
+object MsgInfo extends ChiselEnum {
+  // Credit for Nop only
+  // val DontCare = Value(0x0000.U(16.W))
+  val RegularResponse = Value(0x0000.U(16.W))
+  val ReturnCredit1   = Value(0x0001.U(16.W))
+  val ReturnCredit2   = Value(0x0002.U(16.W))
+  val ReturnCredit3   = Value(0x0003.U(16.W))
+  val ReturnCredit4   = Value(0x0004.U(16.W))
+  val StallResponse   = Value(0x0fff.U(16.W))
+  // val StallResponse   = Value(0xffff.U(16.W))
+  val Stall           = Value(0xffff.U(16.W))
+
+}
--- src/test/scala/DummyModule.scala
+++ src/test/scala/DummyModule.scala
@@ -3,13 +3,111 @@ package edu.berkeley.cs.ucie.digital
 import chisel3._
 import chiseltest._
 import org.scalatest.funspec.AnyFunSpec
+import interfaces._
+import sideband._
+import logphy._
+
+class dut2 (afeParams: AfeParams) extends Module{
+    val io = IO(new Bundle{
+        // sender 
+        val enable = Input(Bool())
+        val ready = Input(Bool())
+        val msgHeader = Input(new SidebandMessageHeader())
+        val data0 = Input(Bits(32.W))
+        val data1 = Input(Bits(32.W))
+        val msgOut = Output(Bits(32.W))
+        val done = Output(Bool())
+        
+        // receiver 
+        val sbAfe = new SidebandAfeIo(afeParams)
+    })
+
+    val sme = Module(new SidebandMessageEncoder())
+
+    sme.io.enable := io.enable
+    sme.io.ready := io.ready
+    sme.io.msgHeaderIn := io.msgHeader
+    sme.io.data0 := io.data0
+    sme.io.data1 := io.data1
+    io.msgOut := sme.io.msgOut
+    io.done := sme.io.done
+
+    io.sbAfe.txData.bits := io.msgOut
+    io.sbAfe.txData.valid := io.enable && !io.done
+    io.sbAfe.pllLock := 1.U
+    io.sbAfe.rxEn := 0.U
+    io.sbAfe.rxData.ready := 0.U
+    // io.sbAfe.txData.irdy := io.sbAfe.txData.valid   
+}
+
+class receiver () extends Module{
+    val io = IO(new Bundle{
+        val ready = Input(Bool())
+        val in = Flipped(Decoupled3(Bits(32.W)))
+        val bits = Output(Bits(32.W))
+    })
+    io.in.ready := io.ready
+    printf(p"${io.in}\n")
+    io.bits := io.in.bits
+}
 
 class DummyModuleTest extends AnyFunSpec with ChiselScalatestTester {
-  describe("DummyModule") {
-    it("should invert its input") {
-      test(new DummyModule()) { c =>
-        c.io.a.poke(true.B)
-        c.io.b.expect(false.B)
+  describe("DUT Module with Decoupled3 Interface") {
+    it("should properly transfer data from sender to receiver when ready and valid") {
+      test(new dut2(AfeParams())) { c =>
+        // Initialize inputs
+        c.io.enable.poke(true.B)  // Example data to send
+        
+        c.io.msgHeader.srcid.poke(1.U)
+        c.io.msgHeader.rsvd_00.poke(1.U)
+        c.io.msgHeader.rsvd_01.poke(1.U)
+        c.io.msgHeader.msgCode.poke(1.U)
+        c.io.msgHeader.rsvd_02.poke(1.U)
+        c.io.msgHeader.opcode.poke(1.U)
+        // Phase 1
+        c.io.msgHeader.dp.poke(1.U)
+        c.io.msgHeader.cp.poke(1.U)
+        c.io.msgHeader.rsvd_10.poke(1.U)
+        c.io.msgHeader.dstid.poke(1.U)
+        c.io.msgHeader.msgInfo.poke(1.U)
+        c.io.msgHeader.msgSubCode.poke(1.U)
+        c.io.data0.poke(5.U)
+        c.io.data1.poke(9.U)
+        
+
+        // // Chisel testbench does not allow .asUInt
+        // c.io.msgHeader.srcid.poke(SourceID.DieToDieAdapter.asUInt)
+        // c.io.msgHeader.rsvd_00.poke(1.U)
+        // c.io.msgHeader.rsvd_01.poke(1.U)
+        // c.io.msgHeader.msgCode.poke(MsgCode.LinkMgmt_RDI_Req.asUInt)
+        // c.io.msgHeader.rsvd_02.poke(1.U)
+        // c.io.msgHeader.opcode.poke(PacketType.MessageWithoutData.asUInt)
+        // // Phase 1
+        // c.io.msgHeader.dp.poke(1.U)
+        // c.io.msgHeader.cp.poke(1.U)
+        // c.io.msgHeader.rsvd_10.poke(1.U)
+        // c.io.msgHeader.dstid.poke(1.U)
+        // c.io.msgHeader.msgInfo.poke(MsgInfo.RegularResponse.asUInt)
+        // c.io.msgHeader.msgSubCode.poke(MsgSubCode.Active.asUInt)
+        // c.io.data0.poke(5.U)
+        // c.io.data1.poke(9.U)
+
+        // Step the clock to observe the transfer
+        c.clock.step(1)
+        println(s"msgOut: ${c.io.msgOut.peek().litValue}, done: ${c.io.done.peek().litToBoolean}")
+        // c.io.msgOut.peek()
+        // c.io.done.peek()
+        c.clock.step(5)
+        println(s"msgOut: ${c.io.msgOut.peek().litValue}, done: ${c.io.done.peek().litToBoolean}")
+
+        // c.io.msgOut.peek()
+        // c.io.done.peek()
+        c.io.enable.poke(false.B)
+        c.clock.step(1)
+        println(s"msgOut: ${c.io.msgOut.peek().litValue}, done: ${c.io.done.peek().litToBoolean}")
+
+        // c.io.msgOut.peek()
+        // c.io.done.peek() 
       }
     }
   }
